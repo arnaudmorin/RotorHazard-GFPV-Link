d6db93f00722b9a4fa9146a7010025dc0913e7f6
Arnaud M <arnaud.choupinette@mailops.fr>
2024-09-08 18:44:43 +0200

    Add new db tables for qualifier
    Signed-off-by: Arnaud M <arnaud.choupinette@mailops.fr>

--- db/create.sql
+++ db/create.sql
@@ -25,3 +25,22 @@ CREATE TABLE `races` (
   KEY `races_ibfk_1` (`eventid`),
   CONSTRAINT `races_ibfk_1` FOREIGN KEY (`eventid`) REFERENCES `events` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci
+
+CREATE TABLE `ranks` (
+  `eventid` varchar(255) NOT NULL,
+  `pilot` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `extra` text DEFAULT NULL,
+  PRIMARY KEY (`pilot`,`eventid`),
+  KEY `ranks_ibfk_1` (`eventid`),
+  CONSTRAINT `ranks_ibfk_1` FOREIGN KEY (`eventid`) REFERENCES `events` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci
+
+CREATE TABLE `laps` (
+  `eventid` varchar(255) NOT NULL,
+  `pilot` varchar(255) NOT NULL,
+  `laps` text DEFAULT NULL,
+  PRIMARY KEY (`pilot`,`eventid`),
+  KEY `laps_ibfk_1` (`eventid`),
+  CONSTRAINT `laps_ibfk_1` FOREIGN KEY (`eventid`) REFERENCES `events` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci
